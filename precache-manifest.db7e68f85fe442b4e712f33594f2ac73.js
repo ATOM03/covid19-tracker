990dcb3aa87cbe37b3c10138ba228d9f0770f98b
ATOM03 <priyanshujhaginia@outlook.com>
2020-04-17 00:09:26 +0530

    Updates

--- precache-manifest.db7e68f85fe442b4e712f33594f2ac73.js
+++ precache-manifest.db7e68f85fe442b4e712f33594f2ac73.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "43fff038eec76bbd4d4c760013c6080b",
+    "revision": "0105791fb28648f4daf15ac97f33fb0b",
     "url": "/covid19-tracker/index.html"
   },
   {
-    "revision": "750a3c312d3e09c45578",
-    "url": "/covid19-tracker/static/css/main.78be76b8.chunk.css"
+    "revision": "382322cd5bf78d99ff0e",
+    "url": "/covid19-tracker/static/css/main.b9e31b83.chunk.css"
   },
   {
     "revision": "a1b89de17371c70d56d6",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid19-tracker/static/js/2.049874f3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "750a3c312d3e09c45578",
-    "url": "/covid19-tracker/static/js/main.da772edb.chunk.js"
+    "revision": "382322cd5bf78d99ff0e",
+    "url": "/covid19-tracker/static/js/main.d497aa38.chunk.js"
   },
   {
     "revision": "e8cdb4966ae05cd4eb5a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19-tracker/precache-manifest.bd9acac92c44f2210ef47549e828c340.js"
+  "/covid19-tracker/precache-manifest.db7e68f85fe442b4e712f33594f2ac73.js"
 );
 
 self.addEventListener('message', (event) => {
