e09b532c4bfec33d1b3acf25595512abbdea5b4e
ATOM03 <priyanshujhaginia@outlook.com>
2020-04-16 17:38:28 +0530

    Updates

---
+++ precache-manifest.5fb19f38f092cc225c46a076ed8d6c49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e745a2d5a6aa5b0a079775e2c6e991",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "6fc6545ab90e3762a845",
+    "url": "/covid19-tracker/static/css/main.95ed5f47.chunk.css"
+  },
+  {
+    "revision": "a1b89de17371c70d56d6",
+    "url": "/covid19-tracker/static/js/2.049874f3.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/covid19-tracker/static/js/2.049874f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fc6545ab90e3762a845",
+    "url": "/covid19-tracker/static/js/main.b9924737.chunk.js"
+  },
+  {
+    "revision": "e8cdb4966ae05cd4eb5a",
+    "url": "/covid19-tracker/static/js/runtime-main.55640149.js"
+  }
+]);
