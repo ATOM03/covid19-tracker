e8ca951f16e4362d20cff03f628fa33e012c230d
PriyanshuJhaginia <priyanshu.jhaginia@albanero.io>
2022-05-09 16:04:11 +0530

    Handles the Error Messages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import StateDate from "./components/State/StateData";
 import Header from "./components/Header/Header";
 import Chart from "./components/Chart/Chart";
 import axios from "axios";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
 class App extends Component {
   constructor() {
     super();
@@ -17,12 +26,15 @@ class App extends Component {
       deltarecovered: "",
       deltadeaths: "",
       case_time: [],
+      open: false,
+      openSucess: false,
+      openError: false,
     };
   }
   async componentDidMount() {
-    const data = await fetch(
-      "https://cors-anywhere.herokuapp.com/https://api.covid19india.org/data.json"
-    )
+    const data = await fetch("https://api.covid19india.org/data.json", {
+      mode: "cors",
+    })
       .then((res) => res.json())
       .then((json) => {
         this.setState({
@@ -35,10 +47,13 @@ class App extends Component {
           deltarecovered: json.statewise[0].deltarecovered,
           deltadeaths: json.statewise[0].deltadeaths,
           case_time: json.cases_time_series,
+          openSucess: true,
+          open: true,
         });
       })
       .catch((e) => {
         console.log(e);
+        this.setState({ openError: true });
       });
 
     return data;
@@ -54,23 +69,67 @@ class App extends Component {
       return (
         <div>
           <h1 className="header">COVID19-INDIA</h1>
-          <div className="flex">
-            <div className="status">
-              <Header
-                confirmed={this.state.confirmed}
-                active={this.state.active}
-                recovered={this.state.recovered}
-                deaths={this.state.deaths}
-                deltaconfirmed={this.state.deltaconfirmed}
-                deltarecovered={this.state.deltarecovered}
-                deltadeaths={this.state.deltadeaths}
-              />
-              <StateDate status={this.state.status} />
+          {this.state.open ? (
+            <div className="flex">
+              <div className="status">
+                <Header
+                  confirmed={this.state.confirmed}
+                  active={this.state.active}
+                  recovered={this.state.recovered}
+                  deaths={this.state.deaths}
+                  deltaconfirmed={this.state.deltaconfirmed}
+                  deltarecovered={this.state.deltarecovered}
+                  deltadeaths={this.state.deltadeaths}
+                />
+                <StateDate status={this.state.status} />
+              </div>
+              <div>
+                <Chart case_time={this.state.case_time} />
+              </div>
             </div>
-            <div>
-              <Chart case_time={this.state.case_time} />
-            </div>
-          </div>
+          ) : (
+            <div className="ErrorDoc">No data Available</div>
+          )}
+
+          <Stack spacing={2}>
+            <Snackbar
+              open={this.state.openSucess}
+              anchorOrigin={{ vertical: "top", horizontal: "right" }}
+              autoHideDuration={6000}
+              onClose={() => {
+                this.setState({ openSucess: false });
+              }}
+            >
+              <Alert
+                onClose={() => {
+                  this.setState({ openSucess: false });
+                }}
+                severity="success"
+                sx={{ width: "100%" }}
+              >
+                Data Fetched Successfully
+              </Alert>
+            </Snackbar>
+
+            <Snackbar
+              open={this.state.openError}
+              anchorOrigin={{ vertical: "top", horizontal: "right" }}
+              autoHideDuration={6000}
+              onClose={() => {
+                this.setState({ openError: false });
+              }}
+            >
+              <Alert
+                onClose={() => {
+                  this.setState({ openError: false });
+                }}
+                severity="error"
+                sx={{ width: "100%" }}
+              >
+                Enable to Fetch the Data
+              </Alert>
+            </Snackbar>
+          </Stack>
         </div>
       );
     }
